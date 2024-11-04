a11cd99f044c3b9f7c76ae82eeb00263284c301e
Pawel <p.awel608@wp.pl>
2024-11-04 18:51:56 +0100

    title css updated

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -6,6 +6,7 @@ export default function Title() {
       <h2>Made by Paweł Kozłowski</h2>
       <div className={styles.icons}>
         <a
+          className={styles.link}
           href="https://github.com/sinet77"
           target="_blank"
           aria-label="GitHub"
@@ -13,6 +14,7 @@ export default function Title() {
           Github
         </a>
         <a
+          className={styles.link}
           href="https://www.linkedin.com/in/pawe%C5%82-koz%C5%82owski-69b29521b/"
           target="_blank"
           aria-label="LinkedIn"
